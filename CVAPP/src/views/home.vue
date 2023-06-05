acec5cae4b8323e0e7878e9dd971033e9d62b5bd
odugbesanyemi <odugbesanyemi@gmail.com>
2023-06-05 11:14:20 +0100

    updated

--- CVAPP/src/views/home.vue
+++ CVAPP/src/views/home.vue
@@ -1,11 +1,13 @@
 <template>
-    <div class="container">
-        <h2 class="">Hello world</h2>
+    <div class="container mt-5 ">
+        <h2 class="bg-primary p-5 rounded-2 fw-bold text-white">Hello world</h2>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis sapiente provident, doloribus alias aliquid nobis nihil obcaecati? Modi facere et unde sed numquam repellat nobis! Totam dicta incidunt laboriosam distinctio?</p>
-        <button class="btn btn-success rounded-none">Continue</button>
-    </div>
+        <button class="btn btn-success rounded-none w-full">Continue <i class="bi bi-arrow-right"></i></button>
+    </div> 
 </template>
 <script>
+
+
 export default {
     
 }
